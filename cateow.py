aa18ae2a87b69a4147cbe342154dff0fe05bb930
Paula Crismaru <paulacrismaru@gmail.com>
2017-07-23 10:09:20 +0300

    Adds parameter for meanie

--- cateow.py
+++ cateow.py
@@ -8,18 +8,20 @@ from cateow import cateow
 
 
 @click.command()
-def be_mean():
+@click.option('--meanie', default=None, help='What kitty will say')
+def be_mean(meanie):
     file_path = os.path.dirname(
         os.path.abspath(inspect.getfile(inspect.currentframe())))
     path = os.sep.join([file_path, 'kitties'])
-    meanies = os.sep.join([file_path, 'cateow', 'meanies'])
+    if meanie is None:
+        meanies_path = os.sep.join([file_path, 'cateow', 'meanies'])
+        meanie = random.choice(open(meanies_path).readlines())
     if os.path.isdir(path):
         file = random.choice(os.listdir(path))
         with open(os.sep.join([path, file]), 'rb') as f:
-            content = f.read()
-        text = random.choice(open(meanies).readlines())
+            kitty = f.read()
 
-        cateow.cateow(text, content)
+        cateow.cateow(meanie, kitty)
 
 
 if __name__ == '__main__':
