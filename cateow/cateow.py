bab49dd3f51cacae5f7ebce226bf0229d0a1a0d9
Paula Crismaru <paulacrismaru@gmail.com>
2017-07-29 22:20:29 +0300

    Add docstrings

--- cateow/cateow.py
+++ cateow/cateow.py
@@ -3,12 +3,25 @@ from __future__ import print_function
 import os
 import random
 
-
 import constants
 import utils
 
 
 def cateow(text, kitty_template):
+    """Creates a kitty using the `kitty_template` with the specified `text`
+    wrapped in a balloon
+
+    Args:
+        text (str): the text the balloon will contain
+        kitty_template (str): a template for the kitty which will be returned
+
+    Returns:
+        The kitty template formated with a balloon containig the specified text
+
+    Raises:
+        CateowException if the formating fails
+    """
+
     balloon = utils.make_balloon(text)
     kitty_template = escape_kitty_template(kitty_template)
     try:
@@ -19,6 +32,20 @@ def cateow(text, kitty_template):
 
 
 def get_meanie(meanies_path):
+    """Returns a random meanie from the file located at `meanies_path`, if it's
+    specified. Otherwise it will retrieve on from the default path.
+
+    Args:
+        meanies_path (str): represents a path to a file containig on each line
+            a phrase which can be randomly chosen to be desplayed in the kitty
+            ballloon
+
+    Returns:
+        A random string from the file located at `meanies_path`
+
+    Raises:
+        CateowException if the `meanies_path` is invalid
+    """
     if meanies_path is None:
         meanies_path = os.sep.join([os.path.dirname(__file__),
                                     constants.MEANIES_FILE_PATH])
@@ -34,6 +61,20 @@ def get_meanie(meanies_path):
 
 
 def get_kitty(kitty_file_path):
+    """Returns the kitty template from the specified file located at
+    `kitty_file_path` or a random one from the default folder containing
+    kitties.
+
+    Args:
+        kitty_file_path (str): path to file containing a kitty template
+
+    Returns:
+        The kitty template, with the special characters escaped, found at the
+        specified file path
+
+    Raises:
+        CateowException if the `kitty_file_path` is invalid
+    """
     if kitty_file_path is None:
         path = os.sep.join([os.path.dirname(__file__), constants.KITTIES_PATH])
         if os.path.isdir(path):
@@ -52,6 +93,14 @@ def get_kitty(kitty_file_path):
 
 
 def escape_kitty_template(kitty_template):
+    """Escapes the special characters found int the constants file that might be
+    found in the specified `kitty_template`
+
+    Args:
+        kitty_template (str): string containing the kitty template
+
+    Returns:
+        The kitty template with the special characters escaped"""
     for character, replacement in constants.ESCAPE_CHARACTERS:
         kitty_template = utils.escape_character(
             kitty_template, character, replacement)
--- cateow/utils.py
+++ cateow/utils.py
@@ -8,6 +8,14 @@ class CateowException(Exception):
 
 
 def split_text_in_lines(text):
+    """Splits text in lines of a length specified in the constants file.
+
+    Args:
+        text (str): the text to be split in lines
+
+    Returns:
+        List of lines.
+    """
     max_len_length = constants.MAX_LEN_LINE
     lines = []
     phrases = text.split('\n')
@@ -26,6 +34,37 @@ def split_text_in_lines(text):
 
 
 def make_balloon(text):
+    """Creates a balloon of a length specified in the constants file, containing
+    the text specified in the parameter. The balloon has top, bottom and
+    margins.
+    The top and the bottom are represented by lines (`-` and `_` for bottom)
+    and the margin from `<`, `>` or `\`, `/` depending on the number of lines
+    in the text.
+
+    Args:
+        text (str): the text the balloon will contain
+
+    Example:
+
+          ___________________________
+        < Lorem ipsum dolor sit amet. >
+          ---------------------------
+         _______________________________________
+        / Lorem ipsum dolor sit amet, consectetur \
+        \ adipiscing elit.                        /
+          ---------------------------------------
+
+          ----------------------------------------
+        / Lorem ipsum dolor sit amet, consectetur \
+        | adipiscing elit. Sed sodales egestas    |
+        | lectus, non porttitor metus cursus nec. |
+        \ Suspendisse potenti.                    /
+          ---------------------------------------
+
+
+    Returns:
+        A balloon containing the text specified.
+    """
     lines = split_text_in_lines(text, constants.MAX_LEN_LINE)
     len_longest_line = max([len(line) for line in lines])
     balloon = []
@@ -55,6 +94,26 @@ def make_balloon(text):
 
 
 def escape_character(input_string, sequence, replace_with, ignore_first=3):
+    """Some kitties might have some characters which need to be escaped.
+    For instance, the characters `{` and `}`. This function replaces those
+    characters with the escaped version of themselves.
+
+    Args:
+        input_string (str): string which contains characters which need to
+            be escaped
+        sequence (str): sequence which needs to be replaced
+        replace_with (str): sequence which will replace `sequence`
+        ignore_first (int, optional): represents the number of first characters
+            which can be ignored. For example, a kitty has in its template the
+            sequences `{balloon}` and `{way}` which represent the parameters
+            used to format the template. Those need not to be escaped. Default
+            value is 3.
+
+    Returns:
+        The `input_string` string with the `sequence` characters replaced with
+        `replace_with` sequence, except for the first `ignore_first`
+        appearences.
+    """
     character_count = input_string.count(sequence)
     if character_count <= ignore_first:
         return input_string
@@ -66,4 +125,12 @@ def escape_character(input_string, sequence, replace_with, ignore_first=3):
 
 
 def get_last_space_index(input_string, start=0):
+    """Finds the last space character in a substring of `input_string`
+    starting from the `start` index.
+
+    Args:
+        input_string (str): string in which the space character will be
+            searched
+        start (int, optional): index showing where the substring starts.
+        Default value is 0."""
     return input_string[start::-1].find(' ')
